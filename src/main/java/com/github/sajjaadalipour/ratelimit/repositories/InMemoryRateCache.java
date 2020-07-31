cf42706a9e47b975194f34007f03c370312bd1d8
Sajjad Alipour <sajjaad.alipour@gmail.com>
2020-07-31 23:16:01 +0430

    Add the InMemoryRateCache Implementation to Rate Limiting

---
+++ src/main/java/com/github/sajjaadalipour/ratelimit/repositories/InMemoryRateCache.java
@@ -0,0 +1,64 @@
+package com.github.sajjaadalipour.ratelimit.repositories;
+
+import com.github.sajjaadalipour.ratelimit.Rate;
+import com.github.sajjaadalipour.ratelimit.RateLimiter;
+import com.github.sajjaadalipour.ratelimit.RatePolicy;
+
+import javax.annotation.Nonnull;
+import java.time.Instant;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * An implementation of {@link RateLimiter} to cache the rate limit data in memory.
+ *
+ * @author Sajjad Alipour
+ */
+public class InMemoryRateCache implements RateLimiter {
+
+    private final ConcurrentHashMap<String, Rate> cache = new ConcurrentHashMap<>();
+
+    /**
+     * Gets the rate limit by the given {@code key} from the {@link #cache} hash map,
+     * then if result is {@code null}, put the new rate to {@link #cache}, else if rate expired
+     * then remove the item from the {@link #cache}. If the rate remaining value be grater than 0,
+     * decrease rate`s remaining and update item.
+     *
+     * @return Encapsulated rate details.
+     */
+    @Override
+    public synchronized Rate consume(@Nonnull RatePolicy ratePolicy) {
+        Optional<Rate> rateOptional = Optional.ofNullable(cache.get(ratePolicy.getKey()));
+
+        if (!rateOptional.isPresent()) {
+            return createRateForFirstTime(ratePolicy);
+        }
+
+        Rate rate = rateOptional.get();
+        if (rate.isExpired()) {
+            cache.remove(ratePolicy.getKey());
+        }
+
+        if (!rate.isExceed()) {
+            rate.decrease();
+
+            if (rate.isExceed() && ratePolicy.getBlockDuration() != null) {
+                final Instant blockedExpiration = Instant.now().plusSeconds(ratePolicy.getBlockDuration().getSeconds());
+                rate.setExpiration(blockedExpiration);
+            }
+
+            cache.put(ratePolicy.getKey(), rate);
+        }
+
+        return rate;
+    }
+
+    private Rate createRateForFirstTime(RatePolicy ratePolicy) {
+        Instant expiration = Instant.now().plusSeconds(ratePolicy.getDuration().getSeconds());
+        Rate rate = new Rate(ratePolicy.getKey(), expiration, ratePolicy.getCount());
+        rate.decrease();
+
+        cache.put(ratePolicy.getKey(), rate);
+        return rate;
+    }
+}
